b41cfff33b9b4417b04abe0abf874597834d614e
Melanie <mmantilla930@gmail.com>
2022-02-01 16:49:11 -0500

    actividad_5

---
+++ Actividad_5/ejercicio2.js
@@ -0,0 +1,13 @@
+var arreglo = []
+var num
+var i =0
+
+while (i<5){
+    i++
+    num = prompt("Agregue numeros al arreglo")
+    arreglo.push(num)
+}
+
+console.log(arreglo)
+
+
---
+++ Actividad_5/ejercicio_1.js
@@ -0,0 +1,11 @@
+arreglo=[1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+lista = []
+
+for(var i=0 ; i < arreglo.length; i++ ){
+  
+    
+    if (arreglo[i]>3){
+        console.log(arreglo[i])
+    }
+}
+
